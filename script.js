8af3e5ff4d2a6a3dababcaa8d6e80f4745526daf
thiago-Parreiras <136278994+thiago-Parreiras@users.noreply.github.com>
2023-06-11 20:38:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-
+function init() {
+    fetch('https://diwserver.vps.webdock.cloud/products')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        let str = ''
+        data = data.products
+        for (let i = 0; i < 10; i++) {
+          let product = data[i]
+          console.log(product)
+          str +=
+            `<div class="row">          
+              <div class="col-7">
+                <p>${product.title}</p>
+              </div>
+              <div class="col-3">
+                <img src="${product.image}"  alt="..." class="img-thumbnail" style="max-width:100px;">
+              </div>
+              <div class="col-2">
+                <a class="btn btn-primary" href="detalhes.html?id=${product.id}" role="button">detalhes</a>
+              </div>
+            </div>`
+        }
+        console.log(str)
+        $('#tela').html(str)
+  
+      })
+      .catch(error => console.log(error));
+    }
